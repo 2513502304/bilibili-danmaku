9b9079e6ca2723254fd9defe3c06588ffe0eece9
翟持江 <129171955+2513502304@users.noreply.github.com>
2025-01-16 13:14:04 +0800

    Update main.py，添加了danmaku数量为0的处理

--- main.py
+++ main.py
@@ -20,14 +20,16 @@ cookie = ''
 if __name__ == "__main__":
     # 从给定的 aid/bvid 与时间段中获取历史弹幕
     dm = get_history_danmaku(bvid=bvid, cookie=cookie)
-
     df = pd.DataFrame(dm)
-    # 将 ctime 时间戳转换为 datetime64[ns]
-    t = pd.to_datetime(df['ctime'].astype(int), unit='s')
-    # 取年月日赋值给 date 字段
-    df['date'] = t.dt.date
-    # 取时分秒赋值给 time 字段
-    df['time'] = t.dt.time
+
+    # 弹幕数不为 0
+    if dm:
+        # 将 ctime 时间戳转换为 datetime64[ns]
+        t = pd.to_datetime(df['ctime'].astype(int), unit='s')
+        # 取年月日赋值给 date 字段
+        df['date'] = t.dt.date
+        # 取时分秒赋值给 time 字段
+        df['time'] = t.dt.time
 
     # 转存
     save_path = './茜特拉莉弹幕.csv'
