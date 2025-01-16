255fe6056c3076aa9b119c1fa9519808934d3eb9
翟持江 <129171955+2513502304@users.noreply.github.com>
2025-01-16 16:06:57 +0800

    Update main.py，修复了当前视频danmaku为0时，转存为.csv文件中出现空索引字段""而非真正意义上的空

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ import pandas as pd
 # 茜特拉莉（bushi，原神怎么你了）
 aid = '113736958350047'  # 视频的 aid，可选。若 aid 为空，则必须提供 bvid 参数
 bvid = 'BV1ft6hYxE75'  # 视频的 bvid，可选。若 bvid 为空，则必须提供 aid 参数
-cid = '27598718983'  # ignore
+cid = '27598718983'  # 视频弹幕的 cid，ignore
 
 # 替换为你自己的 cookie
 cookie = ''
+
+# 转存路径
+save_path = './茜特拉莉弹幕.csv'
 
 if __name__ == "__main__":
     # 从给定的 aid/bvid 与时间段中获取历史弹幕
     dm = get_history_danmaku(bvid=bvid, cookie=cookie)
     df = pd.DataFrame(dm)
-
     # 弹幕数不为 0
     if dm:
         # 将 ctime 时间戳转换为 datetime64[ns]
@@ -30,7 +32,7 @@ if __name__ == "__main__":
         df['date'] = t.dt.date
         # 取时分秒赋值给 time 字段
         df['time'] = t.dt.time
-
-    # 转存
-    save_path = './茜特拉莉弹幕.csv'
-    df.to_csv(save_path)
+        df.to_csv(save_path)
+    # 弹幕数为 0，忽略其索引，以免为空 csv 写入空索引字段 "" 而非真正的空（nothing in this *.csv file）
+    else:
+        df.to_csv(save_path, index=False)
