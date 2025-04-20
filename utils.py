2e55ce4be292b37d0aa4922029f1492779a64551
翟持江 <129171955+2513502304@users.noreply.github.com>
2025-04-20 23:48:51 +0800

    Update utils.py，添加 midHash 反查用户信息功能

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import TypeAlias
+from typing import Callable
 import logging
 
 # 启用调试于 http.client 级别 (requests->urllib3->http.client)
@@ -83,8 +83,115 @@ def bv2av(bvid: str) -> str:
     return (temp & AND) ^ XOR
 
 
-base58encode: TypeAlias = av2bv
-base58decode: TypeAlias = bv2av
+base58encode: Callable[[str], str] = av2bv
+base58decode: Callable[[str], str] = bv2av
+
+# midHash 反查用户信息
+'''
+midHash 反查用户信息
+- bilibili 弹幕中的 midHash 是通过将 uid 经过 CRC32A (ITU I.363.5) 算法进行哈希处理与 16 进制转换后得到的
+- 如 uid = 86137069，经过 hex(crc32(uid)) 转换后的 midHash = 4e902d62
+- 一个简单的方法是采用暴力破解，通过构建存储 uid 和 midHash 对应关系的字典表，并反向查找以确定对应 midHash 的 uid，但这需要大量的存储空间和时间。若想保证查找的准确率和召回率，则需要构建一个至少包含 10 亿个数据的字典表
+- 用于反匿名破解 CRC32A 的高效算法以及 JS 实现：https://github.com/MoePus/bilibili_danmaku_AntiAnonym
+- 考虑到哈希碰撞，可能会出现多个 uid 对应同一个 midHash 的情况，因此反查出的用户信息可能并不准确
+
+CRC32
+- 循环冗余校验是对数据执行完整性检查的常用方法。它是一个简单的哈希函数，可确保数据未损坏，并广泛用于数据通信和数据存储应用程序
+- CRC 不适用于防止有意修改数据。它是一种密码较弱的算法，很容易被逆转
+
+Reference:
+- CRC32：https://www.itu.int/rec/T-REC-I.363.5-199608-I/en
+- 爬虫分析：通过弹幕查找发送者：https://www.bilibili.com/opus/155443177204924005
+- B站弹幕解析：https://zhuanlan.zhihu.com/p/499708255
+'''
+'''将用于反匿名破解 CRC32A 的高效算法以及 JS 实现转换为 Python 实现：https://github.com/MoePus/bilibili_danmaku_AntiAnonym'''
+# CRC32A 多项式
+CRCPOLYNOMIAL = 0xEDB88320
+# CRC32A 查找表
+crctable = [0 for _ in range(256)]
+# 生成 CRC32A 查找表
+for i in range(256):
+    crcreg = i
+    for _ in range(8):
+        if (crcreg & 1) != 0:
+            crcreg = CRCPOLYNOMIAL ^ (crcreg >> 1)
+        else:
+            crcreg = crcreg >> 1
+    crctable[i] = crcreg
+
+
+def crc32(text: str) -> int:
+    crcstart = 0xFFFFFFFF
+    for i in range(len(str(text))):
+        index = (crcstart ^ ord(str(text)[i])) & 0xff
+        crcstart = (crcstart >> 8) ^ crctable[index]
+    return crcstart
+
+
+def crc32_last_index(text: str) -> int:
+    crcstart = 0xFFFFFFFF
+    for i in range(len(str(text))):
+        index = (crcstart ^ ord(str(text)[i])) & 0xff
+        crcstart = (crcstart >> 8) ^ crctable[index]
+    return index
+
+
+def get_crc_index(t: int) -> int:
+    for i in range(256):
+        if crctable[i] >> 24 == t:
+            return i
+    return -1
+
+
+def deep_check(i: int, index: int) -> list[int] | list[int, str]:
+    tc = 0x00
+    text = ''
+    hashcode = crc32(str(i))
+    tc = hashcode & 0xff ^ index[2]
+    if not (tc <= 57 and tc >= 48):
+        return [0]
+    text += str(tc - 48)
+    hashcode = crctable[index[2]] ^ (hashcode >> 8)
+    tc = hashcode & 0xff ^ index[1]
+    if not (tc <= 57 and tc >= 48):
+        return [0]
+    text += str(tc - 48)
+    hashcode = crctable[index[1]] ^ (hashcode >> 8)
+    tc = hashcode & 0xff ^ index[0]
+    if not (tc <= 57 and tc >= 48):
+        return [0]
+    text += str(tc - 48)
+    hashcode = crctable[index[0]] ^ (hashcode >> 8)
+    return [1, text]
+
+
+def crack(text: str) -> str:
+    """
+    反匿名破解 bilibili 弹幕中，经过 CRC32A 算法处理后的 midHash 值，返回对应的 uid
+
+    Args:
+        text (str): midHash 值
+
+    Returns:
+        str: 对应的 uid
+    """
+    index = [0 for _ in range(4)]
+    i = 0
+    ht = int(f'0x{text}', 16) ^ 0xffffffff
+    for i in range(3, -1, -1):
+        index[3 - i] = get_crc_index(ht >> (i * 8))
+        snum = crctable[index[3 - i]]
+        ht ^= snum >> ((3 - i) * 8)
+    for i in range(100_000_000):  #! 1 亿次循环，理论上足够了，若查找不到对应 uid 则放弃查找
+        lastindex = crc32_last_index(i)
+        if lastindex == index[3]:
+            deepCheckData = deep_check(i, index)
+            if deepCheckData[0]:
+                break
+    if i == 100_000_000:
+        return -1
+    return f'{i}{deepCheckData[1]}'
+
 
 if __name__ == '__main__':
     av2bv(aid), bv2av(bvid)
